1265fbca6ce30007aa5656cc4d1def37d19d8dc4
19WH1A1210 <69143336+19WH1A1210@users.noreply.github.com>
2021-06-25 15:20:25 +0530

    Add files via upload

---
+++ JP Lab/Arrays/CharToStr.java
@@ -0,0 +1,9 @@
+public class CharToStr
+{
+   public static void main(String args[])
+   {
+      char[] ch = {'j','a','v','a',' ','p','r','o','g','r','a','m','m','i','n','g'};
+      String str = new String(ch);
+      System.out.println(str);
+   }
+}
